682a8077d0a5ed9e801a4ae5006f134180cc1f86
Wietsevdv <Wietsevdv@users.noreply.github.com>
2023-12-21 12:18:42 +0100

    removed 2 comments

--- Source/GOAPAttempt2/Public/WorldStates.h
+++ Source/GOAPAttempt2/Public/WorldStates.h
@@ -28,8 +28,6 @@ enum class WorldState
 	Chilling
 };
 
-//using Precondition = TPair<WorldStates, bool>;
-//using Consequence = TPair<WorldStates, bool>;
 using Precondition = std::pair<WorldState, bool>;
 using Consequence = std::pair<WorldState, bool>;
 using DesiredState = std::pair<WorldState, bool>;
